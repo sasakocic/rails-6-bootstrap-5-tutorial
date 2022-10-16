b95bc3852266b8f7d78247789597187f4cb96407
Sasa Kocic <sasa.kocic@gmail.com>
2022-10-16 22:54:33 +0200

    added welcome example

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,8 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../js/bootstrap_js_files.js'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-import '../js/bootstrap_js_files.js'
